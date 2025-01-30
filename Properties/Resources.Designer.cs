f7e08053733d158cdf0368ffb5de0e4c80e93a9c
Eli Wilson <ewilson@vacuumtechnology.com>
2025-01-30 10:06:44 -0500

    update icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace SerialDeviceManager.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon serialEmuIcon {
+            get {
+                object obj = ResourceManager.GetObject("serialEmuIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap smcicon {
